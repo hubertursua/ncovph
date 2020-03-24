4760a56f818d3e1e0c9530a63c68c47374bb7058
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-24 10:02:29 +0800

    Updated Data Source text

--- hosting/src/sections/DataSource.tsx
+++ hosting/src/sections/DataSource.tsx
@@ -8,6 +8,9 @@ export default function DataSource(): JSX.Element {
       <Paragraph>
         Data comes from <a href="https://ncovtracker.doh.gov.ph" target="_blank" rel="noopener noreferrer">ncovtracker.doh.gov.ph</a>.
       </Paragraph>
+      <Paragraph>
+        I'm expecting the data to become more incomplete and inaccurate as confirmed cases increase. This is due to the fact that the number of cases will overwhelm the investigators of DOH.
+      </Paragraph>
     </Box>
   );
 }
