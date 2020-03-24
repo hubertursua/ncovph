94da2b4181a596b9bcb491db3b54b1a2e518f1e7
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-24 10:02:43 +0800

    Added donation text in Contributing

--- hosting/src/sections/Contributing.tsx
+++ hosting/src/sections/Contributing.tsx
@@ -13,6 +13,10 @@ export default function Contributing(): JSX.Element {
       <Paragraph>
         <a href="https://github.com/hyubs/ncovph/issues/new?labels=data%20correction" target="_blank" rel="noopener noreferrer">Create a new issue</a> and add the label as <strong>data correction</strong>. Explain why the data is incorrect and provide the correct value if available.
       </Paragraph>
+      <Heading level={3} margin={{ bottom: "xsmall" }}>Donate</Heading>
+      <Paragraph>
+        We need to support our frontliners and those that are deeply affected by the COVID-19 situation. If you can, donate regularly to the institutions organizing charities for these people.
+      </Paragraph>
     </Box>
   );
 }
