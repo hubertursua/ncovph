481a22d2c7cfb3b8bd9b1b8bbd2f1816c86b2c1c
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-26 23:02:28 +0800

    Remove unused code

--- functions/src/apollo.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloServer } from "apollo-server-express";
-import typeDefs from "./typeDefs";
-import resolvers from "./resolvers";
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-export default server;
--- functions/src/typeDefs/index.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "apollo-server-express";
-
-export default gql`
-  type Query {
-    hello: String
-  }
-`;
