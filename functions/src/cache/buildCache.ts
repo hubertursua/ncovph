3d49a36e4b93e8f2ccd97196f18894a1b2596c3a
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-28 09:11:14 +0800

    Type check and error log for buildCache

--- functions/src/cache/buildCache.ts
+++ functions/src/cache/buildCache.ts
@@ -1,12 +1,18 @@
 import storage from "../storage";
 import log from "../utils/log";
 import getAndCacheData from "./getAndCacheData";
+import NodeCache from "node-cache";
 
 export default async function buildCache({
   cache,
   func,
   cacheKey,
   ttl,
+}: {
+  cache: NodeCache;
+  func: Function;
+  cacheKey: string;
+  ttl: number;
 }) {
   let initialState: object = [];
 
@@ -30,6 +36,7 @@ export default async function buildCache({
       try {
         await getAndCacheData(cacheDataOptions);
       } catch (error) {
+        log.error(error);
         cache.set(cacheKey, value, ttl);
       }
     }
