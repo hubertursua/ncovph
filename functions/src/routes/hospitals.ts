83307bac0a93e0982defcaa25023ff24060e115e
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-29 11:44:18 +0800

    Use const for /hospitals instead of direct ArcGIS queries

--- functions/src/routes/hospitals.ts
+++ functions/src/routes/hospitals.ts
@@ -1,8 +1,7 @@
-import cache from "../cache";
+import hospitals from "../consts/Hospitals";
 
 export const path = "/hospitals";
 
 export const handler = async (req, res) => {
-  const cases = cache.get("hospitals");
-  res.json(cases);
+  res.json(hospitals);
 };
