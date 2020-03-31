6637b8d3c0e31ef61e551df57b9de0756a3a6e92
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-31 21:06:45 +0800

    Changed rate limit from 100 per 15 mins to 18 per 3 mins

--- functions/src/routes/rateLimit.ts
+++ functions/src/routes/rateLimit.ts
@@ -1,6 +1,6 @@
 import expressRateLimit from "express-rate-limit";
 
 export const handler = expressRateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 1000 * 60 * 3, // 3 minutes
+  max: 18 // limit each IP to 6 requests per windowMs
 });
