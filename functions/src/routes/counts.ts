42e0fd616ff4a030744c7dbee4faf53d13a9567a
Hyubs Ursua <hyubs.u@gmail.com>
2020-03-31 20:59:19 +0800

    Added arcgis in counts

--- functions/src/routes/counts.ts
+++ functions/src/routes/counts.ts
@@ -1,7 +1,18 @@
+import axios from "axios";
 import cache, { CacheKeys } from "../cache";
 import Hospitals from "../consts/Hospitals";
+import DataUrls from "../consts/DataUrls";
 
 export const path = "/counts";
+
+async function getArcGISCounts(url: DataUrls) {
+  try {
+    const response = await axios.get(url);
+    return response.data.features.length;
+  } catch (error) {
+    return -1;
+  }
+}
 
 export const handler = async (req, res) => {
   res.json({
@@ -9,5 +20,10 @@ export const handler = async (req, res) => {
     "/ofw-cases": (cache.get(CacheKeys.OFW_CASES) as object[]).length,
     "/foreign-national-cases": (cache.get(CacheKeys.FOREIGN_NATIONAL_CASES) as object[]).length,
     "/hospitals": Hospitals.length,
+    arcgis: {
+      PH_masterlist: await getArcGISCounts(DataUrls.NCOVIDTRACKER_LOCAL_CASES),
+      OF_masterlist: await getArcGISCounts(DataUrls.NCOVIDTRACKER_OFW_CASES),
+      FN_masterlist: await getArcGISCounts(DataUrls.NCOVIDTRACKER_FOREIGN_NATIONAL_CASES),
+    }
   });
 };
