dfc1f0eb118b29c553cb2331ae12134ca67c9c12
Hyubs Ursua <hyubs.u@gmail.com>
2020-04-20 12:04:47 +0800

    Rate limit added in env

--- functions/src/middlewares/rateLimit.ts
+++ functions/src/middlewares/rateLimit.ts
@@ -1,8 +1,9 @@
 import expressRateLimit from 'express-rate-limit';
+import environment from '../environment';
 
 // eslint-disable-next-line import/prefer-default-export
 export const handler = expressRateLimit({
-  windowMs: 1000 * 60 * 1, // 1 minutes
-  max: 20, // limit each IP to 20 requests per windowMs
+  windowMs: environment.rateLimit.windowMs,
+  max: environment.rateLimit.max,
   message: { error: 'Too many requests. Slow down.' },
 });
