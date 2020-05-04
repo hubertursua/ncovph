56b98211dd4870169f7cdcf4f6f8047473c67b8f
Hyubs Ursua <hyubs.u@gmail.com>
2020-05-04 20:21:25 +0800

    Increased memory

--- functions/src/services/apollo.ts
+++ functions/src/services/apollo.ts
@@ -5,7 +5,7 @@ import express from '../express';
 export default functions
   .region('us-central1')
   .runWith({
-    memory: '128MB',
+    memory: '256MB',
     timeoutSeconds: 30,
   })
   .https.onRequest(express);
